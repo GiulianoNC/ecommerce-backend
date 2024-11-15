21b9812dc731f91f8649c9187e68e01ec3471704
Giuliano <capovillagiuliano@gmail.com>
2024-11-15 18:34:44 -0300

    actualziacion back end

---
+++ src/config/config.ts
@@ -0,0 +1,5 @@
+export const MERCADO_PAGO_API ='https://api.mercadopago.com/v1'
+export const MERCADO_PAGO_HEADERS ={
+    'Authorization': 'Bearer TEST-4893555842129971-111516-100d52a3d22c26166b6f7d5f40d847f5-521266017',
+    'Content-Type':'application/json'
+}
